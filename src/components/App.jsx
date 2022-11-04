d297d332d2781806b870d622304ea2476af25c07
Szymon Hyziak <szymon.hyziak@protonmail.com>
2022-11-04 17:15:02 +0100

    Switching to methods commonly used in React

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,19 +1,16 @@
-import "./styles/index.css";
-import { poems } from "./poems";
-import { generateRandomNumberInRange } from "isola/utilities";
+import "../styles/index.css";
+
+import { poems } from "../utilities/poems";
+import { drawIndex, drawImage } from "../utilities/utilities";
+import { refresh } from "isola/browser";
 
 import DiceIcon from "./DiceIcon";
 import Header from "./Header";
 import Footer from "./Footer";
 
 export default function App() {
-  function drawPoem() {
-    const index = generateRandomNumberInRange(0, poems.length) - 1;
-    document.getElementById("author").innerText = poems[index].author;
-    document.getElementById("title").innerText = poems[index].title;
-  }
-  window.addEventListener("load", drawPoem);
-
+  const index = drawIndex();
+  let id = 0;
   return (
     <div className="App">
       <Header />
@@ -21,17 +18,19 @@ export default function App() {
         {/* The interactive part starts from here */}
         <article className="controls">
           <section className="name">
-            <p className="author" id="author"></p>
-            <p className="title" id="title"></p>
+            <p className="author">{poems[index].author}</p>
+            <p className="title">{poems[index].title}</p>
           </section>
-          <section className="draw" onClick={drawPoem}>
+          <section className="draw" onClick={refresh}>
             <DiceIcon />
             <p className="caption">Wylosuj kolejny wiersz!</p>
           </section>
         </article>
         <article className="text">
           <h2 className="subheading">Tekst:</h2>
-            <p className="content"></p> 
+          {poems[index].verses.map(verse => {
+            id += 1; return <p className="content" key={id}>{verse}</p>;
+          })}
           <img src="/illustrations/dalle_pumpkin_01.png" alt="" className="image"/>
         </article>
         {/* The interactive part ends up here */}
