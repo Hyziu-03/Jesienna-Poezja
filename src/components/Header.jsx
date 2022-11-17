8829ce69602d69cd6af1c28bdbdc33266af83e83
Szymon Hyziak <szymon.hyziak@protonmail.com>
2022-11-17 18:00:26 +0100

    Making header not refresh the page

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,6 @@
-import { refresh } from "isola/browser";
-
 export default function Header() {
   return (
-    <header className="header" onClick={refresh} tabIndex={0}>
+    <header className="header" tabIndex={0}>
       <h1 className="heading">jesienna.poezja.vercel.app</h1>
     </header>
   );
