8205e98577bad81e2778c68beea5ac7e77a989cf
Szymon Hyziak <szymon.hyziak@protonmail.com>
2022-11-30 19:58:51 +0100

    Caching assets

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,1 +1,47 @@
-try{this.addEventListener("fetch",()=>{})}catch(t){console.error(t)}
+const CACHE_NAME = "poetry-cache";
+
+self.addEventListener("fetch", event => {
+	if (!(event.request.url.indexOf("http") === 0)) return;
+	event.respondWith(
+		fetch(event.request)
+			.then(result => {
+				const resultClone = result.clone();
+				caches
+					.open(CACHE_NAME)
+					.then(cache =>
+						cache.put(
+							event.request,
+							resultClone
+						)
+					);
+				return result;
+			})
+			.catch(error => {
+				caches
+					.match(event.request)
+					.then(result => result);
+			})
+	);
+});
+
+self.addEventListener("activate", event => {
+	const cacheWhitelist = [];
+	cacheWhitelist.push(CACHE_NAME);
+	event.waitUntil(
+		caches
+			.keys()
+			.then(cacheNames =>
+				Promise.all(
+					cacheNames.map(cacheName => {
+						if (
+							!cacheWhitelist.includes(
+								cacheName
+							)
+						)
+							return caches.delete(cacheName);
+					})
+				)
+			)
+			.catch(error => console.error(error))
+	);
+});
