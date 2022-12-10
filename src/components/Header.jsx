570c48a3a57cf94ea5bcce2a9ecb4107925d815a
Szymon Hyziak <szymon.hyziak@protonmail.com>
2022-12-10 18:55:05 +0100

    Correcting how the app looks and adding metadata

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 export default function Header() {
   return (
     <header className="header" tabIndex={0}>
-      <h1 className="heading">jesienna.poezja.vercel.app</h1>
+      <h1 className="heading">KALNICA - Jesienna Poezja</h1>
     </header>
   );
 }
