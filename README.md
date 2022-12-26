e851054d0afbca7af332ba637db56e2109bdd812
Szymon Hyziak <szymon.hyziak@protonmail.com>
2022-12-26 12:32:34 +0100

    Moving poems to a remote database

--- README.md
+++ README.md
@@ -1,13 +1,3 @@
 # Jesienna Poezja
 
 Zbiór jesiennych wierszy
-
-## TO DO
-
-[ ] Add poems, links:
-    - <http://www.zosia.piasta.pl/o-jesieni.htm>
-    - <https://poezja.org/wz/zbior/Wiersze_o_jesieni/>
-    - <https://zinterpretuj.pl/wiersze/o-jesieni/>
-    - <https://exerim.pl/strefa/wiersze-o-jesieni>
-
-[ ] Extract poems to an API
--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,10 @@
 import "../styles/index.css";
 
-import { drawImage, drawIndex } from "../utilities/utilities";
-import { poems } from "../utilities/poems";
-import { initial } from "../utilities/initial-values";
-
 import React, { Suspense, useState } from "react";
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
+import { getDatabase, ref, child, get } from "firebase/database";
+import { getRandomElement, generateRandomNumberInRange } from "isola/utilities";
 
 const Header = React.lazy(() => import("./Header"));
 const Footer = React.lazy(() => import("./Footer"));
@@ -26,18 +24,58 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 
+function drawImage() {
+	try {
+		const illustrations = [
+			"/images/c9YD8Qw2HT.webp", "/images/cqTnZ6R0ft.webp",
+			"/images/EoPMjgRhuR.webp", "/images/F9LRcJIzSA.webp",
+			"/images/FSAPqbdUYz.webp", "/images/gmWh5bnUsl.webp",
+			"/images/gUEluvQrrX.webp", "/images/hHkSPKmjpH.webp",
+			"/images/JlUAAzCuSf.webp", "/images/tEnZcNHW6H.webp",
+			"/images/UmzqhdP0jA.webp", "/images/V83opBusbG.webp",
+			"/images/wFkprGimeJ.webp", "/images/wZyJdLicNB.webp",
+			"/images/zRGvpnvJoZ.webp", "/images/zYdyL0xvWc.webp",
+		];
+		return getRandomElement(illustrations);
+	} catch (error) {
+		console.error(error);
+	}
+}
+
+const initialValue = {
+	author: "Jan Brzechwa",
+	title: "Jesień",
+	verses: {
+		content: [
+			"O, jakie rzewne widowisko: <br/> Czerwone liście za oknami <br/> I cienie brzóz, płynące nisko <br/> Za odbitymi obłokami.",
+			"Pies nie ujada. Zły i chory <br/> Omija cienie października, <br/> Na tykach ciepłe pomidory <br/> Są jak korale u indyka,",
+			"Na babim lecie, zawieszonym <br/> Między drzewami jak antena, <br/> Żałośnie drga wyblakłym tonem <br/> Niepowtarzalna kantylena,",
+			"Rzednąca trawa, blade dzwońce, <br/> Rozklekotane późne świerszcze, <br/> I pomarszczone siwe słońce, <br/> I ja - piszący rzewne wiersze.",
+		],
+		length: 4
+	},
+};
+
 export default function App() {
 	const [index, setIndex] = useState(0);
-	const [author, setAuthor] = useState(initial.author);
-	const [title, setTitle] = useState(initial.title);
-	const [verses, setVerses] = useState(initial.verses.content);
-	const [totalVerses, getTotalVerses] = useState(initial.verses.length);
+	const [author, setAuthor] = useState(initialValue.author);
+	const [title, setTitle] = useState(initialValue.title);
+	const [verses, setVerses] = useState(initialValue.verses.content);
+	const [totalVerses, getTotalVerses] = useState(initialValue.verses.content.length);
 
-	function drawPoem() {
-		setIndex(drawIndex());
-		setAuthor(poems[index].author);
-		setTitle(poems[index].title);
-		setVerses(poems[index].verses);
+	async function drawPoem() {
+		const reference = ref(getDatabase());
+		const drawnIndex = generateRandomNumberInRange(0, 5);
+		setIndex(drawnIndex);
+
+		const path = `${index}`;
+		const snapshot = await get(child(reference, path));
+		if (!snapshot.exists()) return;
+
+		const poem = snapshot.val();
+		setAuthor(poem.author);
+		setTitle(poem.title);
+		setVerses(poem.verses);
 		getTotalVerses(verses.length);
 	}
 
