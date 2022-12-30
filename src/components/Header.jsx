6cb2c505ef57c95f8783b99160a03ad966e35db2
Szymon Hyziak <szymon.hyziak@protonmail.com>
2022-12-30 21:44:16 +0100

    Simplifying JavaScript

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,15 @@
+const path = "../../public/assets/favicon.webp";
+const shortenedTitle = "KALNICA";
+const fullTitle = "KALNICA - Jesienna Poezja";
+
 export default function Header() {
+  const condition = window.innerWidth < 465;
   return (
     <header className="header" tabIndex={0}>
-      <h1 className="heading">KALNICA - Jesienna Poezja</h1>
+      <img src={path} alt="" />
+      <h1 className="heading">
+        {condition ? shortenedTitle : fullTitle}
+      </h1>
     </header>
   );
 }
